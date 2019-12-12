58cdcefde486f7c00d8d654e9d80bbb9ccfab377
Jesse <S1118119@student.windesheim.nl>
2019-12-12 13:50:33 +0100

    Fixed it but need the rest of productpage
    ...

--- pages/product.php
+++ pages/product.php
@@ -9,18 +9,11 @@ $database = new database();
 
 $ShowTemprature = $database->DBQuery('SELECT si.stockitemid, si.stockitemname, ischillerstock,  crt.ColdRoomTemperatureID, crt.temperature FROM stockitems AS si LEFT JOIN coldroomstockitems AS crsi ON si.stockitemid = crsi.stockitemid LEFT JOIN coldroomtemperatures AS crt ON crsi.ColdRoomTemperatureID = crt.ColdRoomTemperatureID WHERE IsChillerStock = ?',[1]);
 
-/*
-$ChillerStock = $ShowTemprature['Chillerstock'];
-$ShowTemprature = $ShowTemprature['temprature'] 
-*/
-print_r($ShowTemprature);
 
 
-
-
-//if ($ShowTemprature['ischillerstock']){
-    print_r($ShowTemprature['crt.temperature']); 
-  //  }
+if ($ShowTemprature[1]['ischillerstock'] == 1){
+    print($ShowTemprature[1]['temperature']. '°C' . " " . $ShowTemprature[1]['stockitemname']); 
+   }
 
 
 $database->closeConnection();
