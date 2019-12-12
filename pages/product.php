fcf6c7932911c0d27938f08869b5aa263a731663
Jesse <S1118119@student.windesheim.nl>
2019-12-12 13:19:58 +0100

    Nog niet af
    FUCK

--- pages/product.php
+++ pages/product.php
@@ -5,9 +5,18 @@ $database = new database();
 
 $ShowTemprature = $database->DBQuery('SELECT si.stockitemid, si.stockitemname, ischillerstock,  crt.ColdRoomTemperatureID, crt.temperature FROM stockitems AS si LEFT JOIN coldroomstockitems AS crsi ON si.stockitemid = crsi.stockitemid LEFT JOIN coldroomtemperatures AS crt ON crsi.ColdRoomTemperatureID = crt.ColdRoomTemperatureID WHERE IsChillerStock = ?',[1]);
 
-If ($ShowTemprature['Chillerstock']=1){
-    print($ShowTemprature['temprature']); 
-    }
+/*
+$ChillerStock = $ShowTemprature['Chillerstock'];
+$ShowTemprature = $ShowTemprature['temprature'] 
+*/
+print_r($ShowTemprature);
+
+
+
+
+//if ($ShowTemprature['ischillerstock']){
+    print_r($ShowTemprature['crt.temperature']); 
+  //  }
 
 
 $database->closeConnection();
