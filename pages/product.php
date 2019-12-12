b3aab5b578703950088fa6a64538319070cf8fc7
Jesse <S1118119@student.windesheim.nl>
2019-12-12 13:51:01 +0100

    Revert "Merge branch 'Filename-Chilledproducts' into Dev"
    This reverts commit 17a72a89e1397976c7bfd25b72fec19b506c1205.

--- pages/product.php
+++ pages/product.php
@@ -3,17 +3,3 @@
  * Kbs project - 2019 © ICTM1o1 - Boaz, Jesse, Jordy, Kahn, Ton
  * View all products
  */
-
-$database = new database();
-
-
-$ShowTemprature = $database->DBQuery('SELECT si.stockitemid, si.stockitemname, ischillerstock,  crt.ColdRoomTemperatureID, crt.temperature FROM stockitems AS si LEFT JOIN coldroomstockitems AS crsi ON si.stockitemid = crsi.stockitemid LEFT JOIN coldroomtemperatures AS crt ON crsi.ColdRoomTemperatureID = crt.ColdRoomTemperatureID WHERE IsChillerStock = ?',[1]);
-
-
-
-if ($ShowTemprature[1]['ischillerstock'] == 1){
-    print($ShowTemprature[1]['temperature']. '°C' . " " . $ShowTemprature[1]['stockitemname']); 
-   }
-
-
-$database->closeConnection();
