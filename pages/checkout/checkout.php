f749ecf6234b56f9e10679d6504288d3b32b0328
Ton <s1128846@student.windesheim.nl>
2019-12-16 14:58:57 +0100

    uitgecommende code verkleint

--- pages/checkout/checkout.php
+++ pages/checkout/checkout.php
@@ -1,53 +1,10 @@
 <?php
 
-?>
-<!-- <form action='/checkout.php' method='POST'>
-<select name='Kies uw bank'>
-    <option>Kies uw bank..</option>
-    <option value='ABN AMRO'>ABN AMRO</option>
-    <option value='ASN Bank'>ASN Bank</option>
-    <option value='Handelsbanken'>Handelsbanken</option>
-    <option value='ING'>ING</option>
-    <option value='Knab'>Knab</option>
-    <option value='Moneyou'>Moneyou</option>
-    <option value='Rabobank'>Rabobank</option>
-    <option value='RegioBank'>RegioBank</option>
-    <option value='SNS'>SNS</option>
-    <option value='Triodos Bank'>Triodos Bank</option>
-    <option value='Van Lanschot'>Van Lanschot</option>
-    <option value='bunq'>bunq</option>
-</select>
-<br>
-<input type='submit' value="Verder">
-</form> -->
-
-<?php
-
-// $abnamro = $_POST['ABN AMRO'];
-// $asnbank = $_POST['ASN Bank'];
-// $handelsbanken = $_POST['Handelsbanken'];
-// $ing = $_POST['ING'];
-// $knab = $_POST['Knab'];
-// $moneyou = $_POST['Moneyou'];
-// $rabobank = $_POST['Rabobank'];
-// $regiobank = $_POST['RegioBank'];
-// $sns = $_POST['SNS'];
-// $triodosbank = $_POST['Triodos Bank'];
-// $vanlanschot = $_POST['Van Lanschot'];
-// $bunq = $_POST['bunq'];
-
-// $verder = $_POST['Verder'];
-
-// if(!isset($verder)){
-//     if(isset($abnamro)){
-//         echo('Gaat u verder');
-//         header('Location: /home');
-//     }else{
-//         echo('Kies een bank!');
-//     }
-
-// }
-
+/* ------------------- OUDE CHECKOUT, NIET GEBRUIKEN, NIET VERWIJDEREN ---------------------*/
+// print("<!-- <form action='/checkout.php' method='POST'><select name='Kies uw bank'><option>Kies uw bank..</option><option value='ABN AMRO'>ABN AMRO</option><option value='ASN Bank'>ASN Bank</option><option value='Handelsbanken'>Handelsbanken</option><option value='ING'>ING</option><option value='Knab'>Knab</option><option value='Moneyou'>Moneyou</option><option value='Rabobank'>Rabobank</option><option value='RegioBank'>RegioBank</option><option value='SNS'>SNS</option><option value='Triodos Bank'>Triodos Bank</option><option value='Van Lanschot'>Van Lanschot</option><option value='bunq'>bunq</option></select><br><input type='submit' value='Verder'></form> -->");
+// $abnamro = $_POST['ABN AMRO'];// $asnbank = $_POST['ASN Bank'];// $handelsbanken = $_POST['Handelsbanken'];// $ing = $_POST['ING'];// $knab = $_POST['Knab'];// $moneyou = $_POST['Moneyou'];// $rabobank = $_POST['Rabobank'];// $regiobank = $_POST['RegioBank'];// $sns = $_POST['SNS'];// $triodosbank = $_POST['Triodos Bank'];// $vanlanschot = $_POST['Van Lanschot'];// $bunq = $_POST['bunq'];// $verder = $_POST['Verder'];
+// if(!isset($verder)){//     if(isset($abnamro)){//         echo('Gaat u verder');//         header('Location: /home');//     }else{//         echo('Kies een bank!');//     }// }
+/* ------------------- OUDE CHECKOUT, NIET GEBRUIKEN, NIET VERWIJDEREN ---------------------*/
 
 /*
  * Make sure to disable the display of errors in production code!
