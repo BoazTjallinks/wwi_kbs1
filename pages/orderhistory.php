9184acea88e0adb6818450eaad232d77462bd0e7
JordyWielaard <jordy.wielaard@windesheim.nl>
2019-12-17 13:21:27 +0100

    Order History progress

--- pages/orderhistory.php
+++ pages/orderhistory.php
@@ -4,101 +4,119 @@ if(!isset($_GET['page'])){
     header('location: /orderhistory?page=1');
 }
 
+
 // if(isset($_SESSION['isloggedIn'])){
 //     $usertoken = $_SESSION['isloggedIn'];
 // }else{
 //     echo 'Please login first to check your order history';
 // }
 
-// $offset = $page * 10 - 10;
-
+$page = $_GET['page'];
+$offset = $page * 10 - 10;
+$orderDetailsAll= $database->DBQuery("SELECT O.OrderID, O.OrderDate, OL.StockItemID, OL.Quantity, S.StockItemName, S.RecommendedRetailPrice, S.TaxRate
+FROM orders O 
+JOIN orderlines OL ON O.OrderID = OL.OrderID
+JOIN stockitems S ON OL.StockItemID = S.StockItemID
+WHERE O.CustomerID = ?
+GROUP BY O.OrderID", [832]);
 $orderDetails= $database->DBQuery("SELECT O.OrderID, O.OrderDate, OL.StockItemID, OL.Quantity, S.StockItemName, S.RecommendedRetailPrice, S.TaxRate
 FROM orders O 
 JOIN orderlines OL ON O.OrderID = OL.OrderID
 JOIN stockitems S ON OL.StockItemID = S.StockItemID
 WHERE O.CustomerID = ?
 GROUP BY O.OrderID
-LIMIT ? OFFSET ? ", [$usertoken, 10, $offset]); 
+LIMIT ? OFFSET ? ", [832, 10, $offset]); 
+
+
 if($orderDetails == '0 results found!'){
     echo 'No orders found.';
 }else{
     for($i = 0; $i < count($orderDetails); $i++){
-        echo $orderDetails[$i]['OrderID']; 
+        $getimg = $database->DBQuery('SELECT * FROM picture WHERE StockItemID = ? AND isPrimary IS NOT NULL', [$orderDetails[$i]['StockItemID']]);
+            if ($getimg == '0 results found!') {
+                $img = '/public/img/products/no-image.png';
+            }
+            else {
+                $img = $getimg[0]['ImagePath'];
+            }
+        echo '<figure class="figure"><img class="img-fluid figure-img wwi-itemimg_nowith" src="'.$img.'"></figure>';
+        echo $orderDetails[$i]['OrderID'];
+        echo $orderDetails[$i]['StockItemName'];
         echo '</br>';
         echo "<a href='/orderdetails?OrderID=".$orderDetails[$i]['OrderID']."'>More details</a>";
     }
 }
+print(count($orderDetailsAll));
+$maxPages = ceil(count($orderDetailsAll) / 10);
+print($maxPages);
+$minPages = 1;
+$pagemin = $page - 1;
+$pageminTwo = $page - 2;
+$pageplusTwo = $page + 2;
+$mpagemin = $maxPages - 1;
+$mpageminTwo = $maxPages - 2;
+$mpageminThree = $maxPages - 3;
+$pageplus = $page + 1;
+$mpageplus = $minPages + 1;
+$mpageplusTwo = $minPages + 2;
+$mpageplusThree = $minPages + 3;
 
-// $page = $_GET['page'];
-// $maxPages = ceil(count($orderDetails) / 10);
-// $minPages = 1;
-// $pagemin = $page - 1;
-// $pageminTwo = $page - 2;
-// $pageplusTwo = $page + 2;
-// $mpagemin = $maxPages - 1;
-// $mpageminTwo = $maxPages - 2;
-// $mpageminThree = $maxPages - 3;
-// $pageplus = $page + 1;
-// $mpageplus = $minPages + 1;
-// $mpageplusTwo = $minPages + 2;
-// $mpageplusThree = $minPages + 3;
-
-// if($maxPages <= $minPages){
-//     $page = 1;
-//     echo 'Disabled';
-// }
-// elseif($page < 1){
-//     header('Location: /home');
-// }
-// elseif($page > $maxPages){
-//     header('Location: /home');
-// }
-// elseif($maxPages >= 2 AND $maxPages <= 4){
-//     for($i = 1; $i <= $maxPages; $i++){
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$i' class='button'>$i</a>";
-//         echo "</br>";
-//     }
+if($maxPages <= $minPages){
+    $page = 1;
+    echo 'Disabled';
+}
+elseif($page < 1){
+    header('Location: /home');
+}
+elseif($page > $maxPages){
+    header('Location: /home');
+}
+elseif($maxPages >= 2 AND $maxPages <= 4){
+    for($i = 1; $i <= $maxPages; $i++){
+        echo "<a href='http://kbs.local/orderhistory?&page=$i' class='button'>$i</a>";
+        echo "</br>";
+    }
     
-// }
-// elseif($maxPages > 4){
-//     if($page <= 3){
-//     echo "<a href='http://kbs.local/categories?catid=$cat&page=1' class='button'>1</a>";
-//     echo "</br>";
-//     echo "<a href='http://kbs.local/categories?catid=$cat&page=$mpageplus' class='button'>$mpageplus</a>";
-//     echo "</br>";
-//     echo "<a href='http://kbs.local/categories?catid=$cat&page=$mpageplusTwo' class='button'>$mpageplusTwo</a>";
-//     echo "</br>";
-//     echo "<a href='http://kbs.local/categories?catid=$cat&page=$mpageplusThree' class='button'>$mpageplusThree</a>";
-//     echo "</br>";
-//     echo "<a href='http://kbs.local/categories?catid=$cat&page=$pageplusTwo' class='button'>...</a>";
-//     echo "<a href='http://kbs.local/categories?catid=$cat&page=$maxPages' class='button'>$maxPages</a>";
-//     }
-//     if ($page >= 4 AND $page <= $maxPages - 3){
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=1' class='button'>1</a>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$pageminTwo' class='button'>...</a>";
-//         echo "</br>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$pagemin' class='button'>$pagemin</a>";
-//         echo "</br>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$page' class='button'>$page</a>";
-//         echo "</br>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$pageplus' class='button'>$pageplus</a>";
-//         echo "</br>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$pageplusTwo' class='button'>...</a>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$maxPages' class='button'>$maxPages</a>";
-//     }
-//     if($page >= $maxPages - 2){
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=1' class='button'>1</a>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$pageminTwo' class='button'>...</a>";
-//         echo "</br>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$mpageminThree' class='button'>$mpageminThree</a>";
-//         echo "</br>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$mpageminTwo' class='button'>$mpageminTwo</a>";
-//         echo "</br>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$mpagemin' class='button'>$mpagemin</a>";
-//         echo "</br>";
-//         echo "<a href='http://kbs.local/categories?catid=$cat&page=$maxPages' class='button'>$maxPages</a>";
-//     }
-// }
+}
+elseif($maxPages > 4){
+    if($page <= 3){
+    echo "<a href='http://kbs.local/orderhistory?&page=1' class='button'>1</a>";
+    echo "</br>";
+    echo "<a href='http://kbs.local/orderhistory?&page=$mpageplus' class='button'>$mpageplus</a>";
+    echo "</br>";
+    echo "<a href='http://kbs.local/orderhistory?&page=$mpageplusTwo' class='button'>$mpageplusTwo</a>";
+    echo "</br>";
+    echo "<a href='http://kbs.local/orderhistory?&page=$mpageplusThree' class='button'>$mpageplusThree</a>";
+    echo "</br>";
+    echo "<a href='http://kbs.local/orderhistory?&page=$pageplusTwo' class='button'>...</a>";
+    echo "<a href='http://kbs.local/orderhistory?&page=$maxPages' class='button'>$maxPages</a>";
+    }
+    if ($page >= 4 AND $page <= $maxPages - 3){
+        echo "<a href='http://kbs.local/orderhistory?&page=1' class='button'>1</a>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$pageminTwo' class='button'>...</a>";
+        echo "</br>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$pagemin' class='button'>$pagemin</a>";
+        echo "</br>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$page' class='button'>$page</a>";
+        echo "</br>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$pageplus' class='button'>$pageplus</a>";
+        echo "</br>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$pageplusTwo' class='button'>...</a>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$maxPages' class='button'>$maxPages</a>";
+    }
+    if($page >= $maxPages - 2){
+        echo "<a href='http://kbs.local/orderhistory?&page=1' class='button'>1</a>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$pageminTwo' class='button'>...</a>";
+        echo "</br>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$mpageminThree' class='button'>$mpageminThree</a>";
+        echo "</br>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$mpageminTwo' class='button'>$mpageminTwo</a>";
+        echo "</br>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$mpagemin' class='button'>$mpagemin</a>";
+        echo "</br>";
+        echo "<a href='http://kbs.local/orderhistory?&page=$maxPages' class='button'>$maxPages</a>";
+    }
+}
  
 $database->closeConnection();
 ?>
