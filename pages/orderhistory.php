06e46ca84a99b48685b161a8fa5164f407153731
JordyWielaard <jordy.wielaard@windesheim.nl>
2019-12-18 13:30:32 +0100

    fixed

--- pages/orderhistory.php
+++ pages/orderhistory.php
@@ -19,7 +19,7 @@ JOIN orderlines OL ON O.OrderID = OL.OrderID
 JOIN stockitems S ON OL.StockItemID = S.StockItemID
 WHERE O.CustomerID = ?
 GROUP BY O.OrderID
-ORDER BY O.OrderDate DESC", [832]);
+ORDER BY O.OrderDate DESC, O.OrderID DESC", [832]);
 
 
 $orderDetails= $database->DBQuery("SELECT O.OrderID, O.OrderDate, OL.StockItemID, OL.Quantity, S.StockItemName, S.RecommendedRetailPrice, S.TaxRate
@@ -28,7 +28,7 @@ JOIN orderlines OL ON O.OrderID = OL.OrderID
 JOIN stockitems S ON OL.StockItemID = S.StockItemID
 WHERE O.CustomerID = ?
 GROUP BY O.OrderID
-ORDER BY O.OrderDate DESC
+ORDER BY O.OrderDate DESC, O.OrderID DESC
 LIMIT ? OFFSET ? ", [832, 10, $offset]); 
 
 $itemsNotSold= $database->DBQuery("SELECT DISTINCT SA.StockItemName, SA.RecommendedRetailPrice, SA.TaxRate FROM orders O JOIN orderlines OL ON O.OrderID = OL.OrderID JOIN stockitems_archive SA ON OL.StockItemID = SA.StockItemID WHERE O.CustomerID = ?", [832]);
@@ -53,13 +53,12 @@ if($orderDetails == '0 results found!'){
             echo $itemsNotSold[$i]['StockItemName'];
         }
         echo $orderDetails[$i]['OrderDate'];
-        echo '</br>';
         echo "<a href='/orderdetails?OrderID=".$orderDetails[$i]['OrderID']."'>More details</a>";
+        echo '</br>';
     }
 }
-print(count($orderDetailsAll));
+
 $maxPages = ceil(count($orderDetailsAll) / 7);
-print($maxPages);
 $minPages = 1;
 $pagemin = $page - 1;
 $pageminTwo = $page - 2;
@@ -72,6 +71,7 @@ $mpageplus = $minPages + 1;
 $mpageplusTwo = $minPages + 2;
 $mpageplusThree = $minPages + 3;
 
+echo '</br>';
 if($maxPages <= $minPages){
     $page = 1;
     echo 'Disabled';
@@ -91,39 +91,27 @@ elseif($maxPages >= 2 AND $maxPages <= 4){
 elseif($maxPages > 4){
     if($page <= 3){
     echo "<a href='http://kbs.local/orderhistory?&page=1' class='button'>1</a>";
-    echo "</br>";
     echo "<a href='http://kbs.local/orderhistory?&page=$mpageplus' class='button'>$mpageplus</a>";
-    echo "</br>";
     echo "<a href='http://kbs.local/orderhistory?&page=$mpageplusTwo' class='button'>$mpageplusTwo</a>";
-    echo "</br>";
     echo "<a href='http://kbs.local/orderhistory?&page=$mpageplusThree' class='button'>$mpageplusThree</a>";
-    echo "</br>";
     echo "<a href='http://kbs.local/orderhistory?&page=$pageplusTwo' class='button'>...</a>";
     echo "<a href='http://kbs.local/orderhistory?&page=$maxPages' class='button'>$maxPages</a>";
     }
     if ($page >= 4 AND $page <= $maxPages - 3){
         echo "<a href='http://kbs.local/orderhistory?&page=1' class='button'>1</a>";
         echo "<a href='http://kbs.local/orderhistory?&page=$pageminTwo' class='button'>...</a>";
-        echo "</br>";
         echo "<a href='http://kbs.local/orderhistory?&page=$pagemin' class='button'>$pagemin</a>";
-        echo "</br>";
         echo "<a href='http://kbs.local/orderhistory?&page=$page' class='button'>$page</a>";
-        echo "</br>";
         echo "<a href='http://kbs.local/orderhistory?&page=$pageplus' class='button'>$pageplus</a>";
-        echo "</br>";
         echo "<a href='http://kbs.local/orderhistory?&page=$pageplusTwo' class='button'>...</a>";
         echo "<a href='http://kbs.local/orderhistory?&page=$maxPages' class='button'>$maxPages</a>";
     }
     if($page >= $maxPages - 2){
         echo "<a href='http://kbs.local/orderhistory?&page=1' class='button'>1</a>";
         echo "<a href='http://kbs.local/orderhistory?&page=$pageminTwo' class='button'>...</a>";
-        echo "</br>";
         echo "<a href='http://kbs.local/orderhistory?&page=$mpageminThree' class='button'>$mpageminThree</a>";
-        echo "</br>";
         echo "<a href='http://kbs.local/orderhistory?&page=$mpageminTwo' class='button'>$mpageminTwo</a>";
-        echo "</br>";
         echo "<a href='http://kbs.local/orderhistory?&page=$mpagemin' class='button'>$mpagemin</a>";
-        echo "</br>";
         echo "<a href='http://kbs.local/orderhistory?&page=$maxPages' class='button'>$maxPages</a>";
     }
 }
