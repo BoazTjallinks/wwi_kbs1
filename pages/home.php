7faa925d640c6bdf52e57ddac7fc4ee0de0b97b2
Jesse <S1118119@student.windesheim.nl>
2019-12-18 15:29:56 +0100

    Working Deals
    ...

--- pages/home.php
+++ pages/home.php
@@ -8,86 +8,82 @@
 
 $database = new database();
 
+//Laad StockCategories in 
+$getstockgroups = $database->DBQuery("SELECT stockgroupname, stockgroupID FROM stockgroups WHERE stockgroupID != ?",[2]);
+
+for($i=0; $i<count($getstockgroups); $i++){
+    print($getstockgroups[$i]['stockgroupname']);
+    }
+
+
 // Laad Discounted product zien op home pagina (unfinished) 
+// Dit is de werkende versie die filtered op de datum van nu DESCRIPTION toevogen
 
-
-/*
-$discountedItems = $database->DBQuery("SELECT StartDate, EndDate, DiscountAmount, DiscountPercentage, StockItemName, SI.StockItemID, SI.RecommendedRetailPrice	
+$discountedItems = $database->DBQuery("SELECT StartDate, EndDate, DiscountAmount, DiscountPercentage, StockItemName, SD.StockItemID, SI.RecommendedRetailPrice	
 FROM specialdeals AS SD 
-JOIN stockitems AS SI 
+LEFT JOIN stockitems AS SI 
 ON SD.StockItemID = SI.StockItemID 
 WHERE UTC_DATE BETWEEN StartDate AND EndDate AND StartDate != ?",[1]);
+
+    
+// BEREKENT PRIJS CORRECT
+/*
+for($b=0; $b < 6; $b++){
+    $discountPrice= ($discountedItems[$b]['RecommendedRetailPrice'] - $discountedItems[$b]['DiscountAmount']);
+    print($discountPrice);
+    }
 */
 
+
+//TEST QUERYS
 /*
-$discountedItems = $database->DBQuery("SELECT StartDate, EndDate, DiscountAmount, DiscountPercentage, DealDiscription,  SI.StockItemName, SI.StockItemID, SI.RecommendedRetailPrice
+
+//deze laad bestaande deals in  
+$discountedItems = $database->DBQuery("SELECT StartDate, EndDate, DiscountAmount, DiscountPercentage, SI.StockItemName, SI.StockItemID, SI.RecommendedRetailPrice
 FROM specialdeals AS SD 
 LEFT JOIN stockitems AS SI 
 ON SD.StockItemID = SI.StockItemID
-WHERE  StartDate != ?",[8]);
+WHERE  StartDate != ?",[0]);
+
+print($discountedItems[1]['StockItemName']); // Deze is niet bestaand en laat niets zien
+//print($discountedItems[1]['StartDate']);     // deze bestsaat wel
+
+// Test Query    voor korting berekenen
+
+$TestQuery= $database->DBQuery("SELECT RecommendedRetailPrice, UnitPrice FROM stockitems WHERE UnitPrice != ?;",[0]);
+if(isset($TestQuery[1]['RecommendedRetailPrice'])){
+$TestQuerySom = ($TestQuery[1]['RecommendedRetailPrice'] - $TestQuery[1]['UnitPrice']);
+print($TestQuerySom);
+  }
+
+
+//berekent de prijs met korting maar niet correcte versie {
+    for($b=0; $b < 6; $b++){
+$discountPrice= ($TestQuery[$b]['RecommendedRetailPrice'] - $TestQuery[$b]['UnitPrice']);
+print($discountPrice);
+    }
 */
-/*
-$InTheNameOfPHPTestQuery= $database-->DBQuery("SELECT RecommendedRetailPrice, UnitPrice FROM StockItems" ,[]);
-//var_dump($discountedItems);
 
-if(isset($InTheNameOfPHPTestQuery[8]['RecommendedRetailPrice'])){
-$InTheNameOfPHPTestQuery = ($InTheNameOfPHPTestQuery[8]['RecommendedRetailPrice'] - $InTheNameOfPHPTestQuery[8]['UnitPrice']);
-print($InTheNameOfPHPTestQuery);
-}
-//$discountPrice = ($discountedItems[]['RecommendedRetailPrice'] - $discountedItems[]['DiscountAmount']);
-
-*/
-
-
-//print($discountedItems[1]['StockItemName']);
-//print($discountedItems[1]['StartDate']);    
-/*
-
-//berekent de prijs met korting
-$discountPrice= $row['SI.UnitPrice'] - $row['DiscountAmount'];
-
-// Calculates the remaining time of the deal duration //
-
-$dealDayRemaining=round((($dealTime/24)/60)/60);
-$dealTime =($row['EndDate'] - date('Y-m-d'));
-print($dealTime);
-
-*/
 
 // (unfinished laad korting producten nog niet in) 
-/*
-if($discountedItems == 0){
+
+if($discountedItems !== 0  && $discountedItems[0]['StockItemID'] !== 0){
+    for($b=0; $b < 6; $b++){
+        $discountPrice = ($discountedItems[$b]['RecommendedRetailPrice'] - $discountedItems[$b]['DiscountAmount']);
+        print($discountPrice);
+    }
+}else{  
     print("<ul>");
     print('<li> new items soon </li>');
-    print('<li> new items soon </li>');    
-    print('<li> new items soon </li>');
-    print('<li> new items soon </li>');
-    print('<li> new items soon </li>');
-    print('<li> new items soon </li>');        
+    print('<li> new items soon </li>');           
     print("</ul>");
-}else{ 
-    for ($z=0; $z < 6; $z++){
-
-        $getimg = $database->DBQuery('SELECT * FROM picture WHERE stockitemid = ? AND isPrimary IS NOT NULL', [$discountedItems[$z]['stockitemid']]);
-        if ($getimg == '0 results found!') {
-            $img = '/public/img/products/no-image.png';
-        }
-        else {
-            $img = $getimg[0]['ImagePath'];
-        }
-        
-        showItem($discountedItems[$z]['stockitemid'], $img, $discountedItems[$z]['stockitemname'], '', $discountedItems[$z]['searchdetails'], $discountedItems[$z]['recommendedretailprice']);    
-
-}
 }
 
-*/
 
 
-// populaire producten (geen description)
+// populaire producten
 $PopularProducts = $database->DBQuery("SELECT stockitemname, recommendedretailprice, searchdetails,  ol.stockitemid, count(ol.stockitemid) AS aantal FROM orderlines AS ol JOIN stockitems AS si ON ol.stockitemid = si.stockitemid GROUP BY ol.stockitemid ORDER BY aantal DESC LIMIT ?" ,[6]);
 
-//$PopulairProducts = 0;
 
 if($PopularProducts == !0){
 for($a=0; $a < 6; $a++){
@@ -104,9 +100,6 @@ showItem($PopularProducts[$a]['stockitemid'], $img, $PopularProducts[$a]['stocki
 }
 }else{
     print("Popular products are temporarily unavailable ");
-}
-
-
-
+} 
 
 
