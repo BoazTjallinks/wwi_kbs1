a2d866c6cf095604f456fc4a14cbaea38672ba27
Boaz Tjallinks <blltjalllinks@gmail.com>
2020-01-02 17:14:38 +0100

    Fixed checkout fully!

--- pages/checkout.php
+++ pages/checkout.php
@@ -175,19 +175,23 @@ if ((isset($_POST['submit_ideal']) || isset($_POST['submit_credit'])) && !(isset
                 $shoppedID = $_SESSION['shoppingCart'][$i]['ItemID'];
                 $shoppedAmount = $_SESSION['shoppingCart'][$i]['ItemAmount'];
                         
+                $ordersAmount = $database->DBQuery('SELECT OrderLineID FROM orderlines ORDER BY OrderLineID DESC LIMIT 1', []);
+
+                $OrderLineID = ($ordersAmount[0]['OrderLineID'] + 1);
                 $itemsToSubtract = $database->DBQuery("SELECT StockItemID, QuantityOnHand FROM quantity_test WHERE stockitemid = ?", [$shoppedID]);
                 
                 $instock = $itemsToSubtract[0]['QuantityOnHand'];
                 $newinstock = $instock - $shoppedAmount;
+
                 
                 $database->DBQuery("UPDATE quantity_test SET QuantityOnHand = ? WHERE StockItemID = ?", [$newinstock, $shoppedID]);
                         
                 //-------------- CODE OM DE ORDER IN DE DATABASE TE GOOIEN --------------=
                 $stockitemcolumns = $database->DBQuery("SELECT StockItemName, OuterPackageID, RecommendedRetailPrice, TaxRate FROM stockitems WHERE StockItemID = ?", [$_SESSION['shoppingCart'][$i]['ItemID']]);
                 
-                $array = [$newOrderId, $shoppedID, $stockitemcolumns[0]['StockItemName'], $stockitemcolumns[0]['OuterPackageID'], $shoppedAmount,$stockitemcolumns[0]['RecommendedRetailPrice'], $stockitemcolumns[0]['TaxRate'], $shoppedAmount, $date, 7, $date];
+                $array = [$OrderLineID, $newOrderId, $shoppedID, $stockitemcolumns[0]['StockItemName'], $stockitemcolumns[0]['OuterPackageID'], $shoppedAmount,$stockitemcolumns[0]['RecommendedRetailPrice'], $stockitemcolumns[0]['TaxRate'], $shoppedAmount, $date, 7, $date];
                 
-                $updateDatabaseStock = $database->DBQuery("INSERT INTO orderlines (OrderID, StockItemID, Description, PackageTypeID, Quantity, UnitPrice, TaxRate, PickedQuantity, PickingCompletedWhen, LastEditedBy, LastEditedWhen) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $array);
+                $insertOrderLine = $database->DBQuery("INSERT INTO orderlines (OrderLineID, OrderID, StockItemID, Description, PackageTypeID, Quantity, UnitPrice, TaxRate, PickedQuantity, PickingCompletedWhen, LastEditedBy, LastEditedWhen) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $array);
             }
         }
 
